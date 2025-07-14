34386fd65b4a2443d2c4ea1af97593064bc84f3f
JuanBarbosaMora <portelnom0308@gmail.com>
2025-07-14 16:01:02 -0500

    ADD: first commit whith open ai

---
+++ src/main/java/com/juanba/eco_products_ai/EcoProductsAiApplication.java
@@ -0,0 +1,13 @@
+package com.juanba.eco_products_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoProductsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoProductsAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanba/eco_products_ai/EcoProductsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanba.eco_products_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoProductsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
